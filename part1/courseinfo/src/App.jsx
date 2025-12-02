6a9f6febc849200265e91c283ba6c310c23516c2
Gulcan Damdelen <zipwire1@hotmail.com>
2025-12-02 11:17:20 +0200

    Complete exercise 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -21,12 +21,14 @@ const Content = (props) => {
 }
 
 const Total = (props) => {
-  return <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+  const total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises;
+  return <p>Number of exercises {total}</p>
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
       {
         name: 'Fundamentals of React',
         exercises: 10
@@ -39,13 +41,14 @@ const App = () => {
         name: 'State of a component',
         exercises: 14
       }
-  ];
+    ]
+  };
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
