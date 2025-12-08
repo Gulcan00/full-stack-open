313638542cf991dcd85ae8e8cd7b757437022247
Gulcan Damdelen <gdamdelen@ccc.net>
2025-12-08 11:50:49 +0200

    Split part1 exercises into separate projects (courseinfo, unicafe, anecdotes)

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,51 +1,54 @@
-import { useState } from 'react'
+const Header = (props) => {
+  return <h1>{props.course}</h1>
+}
+
+const Part = (props) => {
+  return (
+    <p>
+        {props.part} {props.exercises}
+    </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.parts[0].name} exercises={props.parts[0].exercises}/>
+      <Part part={props.parts[1].name} exercises={props.parts[1].exercises}/>
+      <Part part={props.parts[2].name} exercises={props.parts[2].exercises}/>
+    </>
+  )
+}
+
+const Total = (props) => {
+  const total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises;
+  return <p>Number of exercises {total}</p>
+}
 
 const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ];
-   
-  const [selected, setSelected] = useState(0);
-  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
-
-
-  const handleNextAnecdoteClick = () => {
-    const randNum = Math.floor(Math.random() * anecdotes.length);
-    setSelected(randNum);
-  }
-
-  const handleVoteClick = () => {
-    const votesCopy = [...votes];
-    votesCopy[selected] += 1;
-    setVotes(votesCopy); 
-  }
-
-  const highestVotes = votes.reduce((obj, curr, index) => {
-    if (curr > obj.max) {
-      obj.max = curr;
-      obj.index = index;
-    }
-    return obj;
-  }, 
-  {max: votes[0], index: 0}
-);
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  };
 
   return (
     <div>
-      <h1>Anecdote of the day</h1>
-      <p>{anecdotes[selected]}</p>
-      <p>has {votes[selected]} votes</p>
-      <button onClick={handleVoteClick}>vote</button>
-      <button onClick={handleNextAnecdoteClick}>next anecdote</button>
-      <h1>Anecdote with most votes</h1>
-      <p>{anecdotes[highestVotes.index]}</p>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
