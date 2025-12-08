0ac8d92b4f62aefb33853260611bd53e3eb40f0d
Gulcan Damdelen <zipwire1@hotmail.com>
2025-12-08 12:35:25 +0200

    Complete exercise 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,31 +1,4 @@
-const Course = ({course}) => {
-  return (
-    <>
-    <Header course={course.name} />
-    <Content parts={course.parts} />
-    <Total parts={course.parts} />
-    </>
-  )
-}
-
-const Header = (props) => <h1>{props.course}</h1>
-
-const Content = ({parts}) => (
-  <div>
-    {parts.map(part => <Part key={part.id} part={part}/>)}
-  </div>
-)
-
-const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-)
-
-const Total = (props) => {
-  const total = props.parts.reduce((sum, part) => sum + part.exercises, 0);
-  return <strong>total of {total} exercises</strong>
-}
+import Course from "./components/Course";
 
 const App = () => {
    const courses = [
