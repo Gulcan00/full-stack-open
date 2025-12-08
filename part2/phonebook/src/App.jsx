d13f97b67389d0887790e4441c4bcdf97743be05
Gulcan Damdelen <gdamdelen@ccc.net>
2025-12-08 13:44:00 +0200

    2.9 Search people by name

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas',
-      number: '040-1234567'
-    }
+   { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -16,6 +17,10 @@ const App = () => {
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
+  }
+
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
   }
 
   const addPerson = (event) => {
@@ -33,12 +38,16 @@ const App = () => {
     }
   }
 
+  const filteredPersons = persons.filter(person => person.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()));
+
   return (
     <div>
       <h2>Phonebook</h2>
+       filter shown with: <input value={search} onChange={handleSearchChange}/>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNameChange}/>
+          name: <input value={newName} onChange={handleNameChange}/><br />
           number: <input value={newNumber} onChange={handleNumberChange}/>
         </div>
         <div>
@@ -47,7 +56,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {
-        persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)
+        filteredPersons.map(person => <p key={person.name}>{person.name} {person.number}</p>)
       }
     </div>
   )
