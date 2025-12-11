31cac3e01c50333d30ed43a052bcc4180546f898
Gulcan Damdelen <gdamdelen@ccc.net>
2025-12-11 14:46:45 +0200

    Complete exercise 2.20

--- part2/countries/src/components/Country.jsx
+++ part2/countries/src/components/Country.jsx
@@ -1,4 +1,17 @@
-const Country = ({country, show}) => {
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const Country = ({country, showWeather}) => {
+    const [weather, setWeather] = useState(null);
+
+    useEffect(() => {
+        if (showWeather) {
+            axios
+            .get(`http://api.openweathermap.org/data/2.5/weather?q=${country.capital[0]}&APPID=${import.meta.env.VITE_API_KEY}&units=metric`)
+            .then(response => setWeather(response.data));
+        }
+    }, []);
+
     return (
         <>
         <h1>{country.name.common}</h1>
@@ -9,6 +22,14 @@ const Country = ({country, show}) => {
             {Object.entries(country.languages).map(([key, value]) => <li key={key}>{value}</li>)}
         </ul>
         <img src={country.flags.png} alt={`Flag of ${country.name.common}`} />
+        {showWeather && weather && (
+            <>
+                <h2>Weather in {country.capital[0]}</h2>
+                <p>Temperature {weather.main.temp} Celsius</p>
+                <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+                <p>Wind {weather.wind.speed} m/s</p>
+            </>
+        )}
         </>
     )
 }
