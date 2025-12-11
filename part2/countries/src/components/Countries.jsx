0af0dd8658daf5b0b352c64a22f3b93b8062bffb
Gulcan Damdelen <zipwire1@hotmail.com>
2025-12-11 11:17:37 +0200

    Complete exercise 2.19

--- part2/countries/src/components/Countries.jsx
+++ part2/countries/src/components/Countries.jsx
@@ -5,6 +5,17 @@ import Country from "./Country";
 
 const Countries = ({search}) => {
     const [countries, setCountries] = useState([]);
+    const [displayedCountries, setDisplayedCountries] = useState(new Set());
+
+    const handleShowCountry = (countryId) => {
+        const displayedCountriesSet = new Set(displayedCountries);
+        if (displayedCountriesSet.has(countryId)) {
+            displayedCountriesSet.delete(countryId);
+        } else {
+            displayedCountriesSet.add(countryId);
+        }
+        setDisplayedCountries(displayedCountriesSet);
+    }
 
     useEffect(() => {
         getAll()
@@ -16,9 +27,24 @@ const Countries = ({search}) => {
     return (
         <div>
             {
+                filteredCountries.length > 0 && (
                 filteredCountries.length > 10 ? <p>Too many matches specify another filter</p> :
-                filteredCountries.length > 1 ? <ul>{filteredCountries.map(country => <li key={country.cca3}>{country.name.common}</li>)}</ul> :
+                  filteredCountries.length > 1 ? 
+                  <ul>
+                    {
+                        filteredCountries.map(country => {
+                        return (
+                        <li key={country.cca3}>
+                            {country.name.common} 
+                            <button onClick={() => handleShowCountry(country.cca3)}>{displayedCountries.has(country.cca3) ? 'Hide': 'Show'}</button>
+                            { (displayedCountries.has(country.cca3)) ? <Country country={country}/> : ''}
+                        </li>
+                        )
+                        })
+                    }
+                </ul> :
                 <Country country={filteredCountries[0]} />
+                )
             }
         </div>
     )
