c2112e4cc2a2b6175dbe064bd1e1a52a20c2a8c5
Gulcan Damdelen <gdamdelen@ccc.net>
2025-12-16 15:46:24 +0200

    3.8 Show req data in morgan middleware

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -26,7 +26,18 @@ let phonebook = [
 ];
 
 app.use(express.json());
-app.use(morgan('tiny'));
+
+const customMorganFormat = morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms', 
+    JSON.stringify(req.body)
+  ].join(' ')
+});
+app.use(customMorganFormat);
 
 app.get('/api/persons', (req, res) => {
     return res.json(phonebook);
