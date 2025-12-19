a5366d57bb49ca3ea0c1ac1e0a3f0cdb086d0bc3
Gulcan Damdelen <zipwire1@hotmail.com>
2025-12-19 15:53:18 +0200

    3.20 Validate phone number

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -17,7 +17,15 @@ const personSchema = mongoose.Schema({
         minLength: 3,
         required: true
     },
-    number: String
+    number: {
+        type: String,
+        validate: {
+            validator: (v) => {
+                return /^\d{2,3}-\d{6,}$/.test(v)
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        }
+    }
 });
 
 personSchema.set('toJSON', {
