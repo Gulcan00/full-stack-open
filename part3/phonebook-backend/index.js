02f301b02440fa901c95bc67ce05bbe63b5b8ca5
Gulcan Damdelen <gdamdelen@ccc.net>
2025-12-19 13:55:57 +0200

    Complete exercise 3.13

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,29 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-const app = express();
+const Person = require('./models/person');
 
-let phonebook = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-];
+const app = express();
 
 app.use(express.static('dist'));
 app.use(express.json());
@@ -41,18 +21,16 @@ const customMorganFormat = morgan(function (tokens, req, res) {
 app.use(customMorganFormat);
 
 app.get('/api/persons', (req, res) => {
-    return res.json(phonebook);
+    Person.find({}).then(persons => {
+      return res.json(persons);
+    })
 });
 
 app.get('/api/persons/:id', (req, res) => {
   const id = req.params.id;
-  const person = phonebook.find(p => p.id === id);
-
-  if (person) {
-    return res.json(person);
-  } else {
-    return res.status(404).end();
-  }
+  Person.findById(id).then(person => {
+     return res.json(person);
+  })
 });
 
 app.delete('/api/persons/:id', (req, res) => {
@@ -101,7 +79,7 @@ app.get('/info', (req, res) => {
     `);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`);
 });
