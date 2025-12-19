4ec484370124d7541689d18cd12049139c756e1f
Gulcan Damdelen <gdamdelen@ccc.net>
2025-12-19 19:33:44 +0200

    3.22 Set up eslint and fix errors

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -1,39 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
 mongoose.set('strictQuery',false)
 
 mongoose.connect(url, { family: 4 })
-    .then(() => console.log('connected to MongoDB'))
-    .catch(error => {
-        console.log('error connecting to MongoDB: ', error.message);
-    })
+  .then(() => console.log('connected to MongoDB'))
+  .catch(error => {
+    console.log('error connecting to MongoDB: ', error.message)
+  })
 
 
 const personSchema = mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
-    },
-    number: {
-        type: String,
-        validate: {
-            validator: (v) => {
-                return /^\d{2,3}-\d{6,}$/.test(v)
-            },
-            message: props => `${props.value} is not a valid phone number!`
-        }
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: (v) => {
+        return /^\d{2,3}-\d{6,}$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
     }
-});
+  }
+})
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
+  transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
   }
 })
 
-module.exports = mongoose.model('Person', personSchema);
+module.exports = mongoose.model('Person', personSchema)
--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
   console.log('give password as argument')
@@ -14,30 +14,30 @@ mongoose.set('strictQuery',false)
 mongoose.connect(url, { family: 4 })
 
 const personSchema = mongoose.Schema({
-    name: String,
-    number: String
-});
+  name: String,
+  number: String
+})
 
-const Person = mongoose.model('Person', personSchema);
+const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 3) {
-    Person.find({}).then(people => {
-        console.log('phonebook');
-        people.forEach(person => {
-            console.log(`${person.name} ${person.number}`);
-        });
-        mongoose.connection.close();
-    });
+  Person.find({}).then(people => {
+    console.log('phonebook')
+    people.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
 } else if (process.argv.length === 5) {
-    const name = process.argv[3];
-    const number = process.argv[4];
-    const person = new Person({
-        name,
-        number
-    });
+  const name = process.argv[3]
+  const number = process.argv[4]
+  const person = new Person({
+    name,
+    number
+  })
 
-    person.save().then(result => {
-        console.log(`added ${result.name} number ${result.number} to phonebook`);
-        mongoose.connection.close();        
-    });
+  person.save().then(result => {
+    console.log(`added ${result.name} number ${result.number} to phonebook`)
+    mongoose.connection.close()
+  })
 }
