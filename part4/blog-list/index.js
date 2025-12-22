e73505d2c45d3d414094556888ae1b3d6c83db66
Gulcan Damdelen <zipwire1@hotmail.com>
2025-12-22 11:49:09 +0200

    4.2 Refactor into modules

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,17 +1,7 @@
-require('dotenv').config();
-const express = require('express')
-const mongoose = require('mongoose')
-const blogRouter = require('./controllers/blogs');
+const app = require("./app")
+const config = require("./utils/config")
 
-const app = express()
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl, { family: 4 })
-
-app.use(express.json())
-app.use('/', blogRouter)
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
