6633437e57f849b8cfe97244c1276b8c97fbd25c
Gulcan Damdelen <gdamdelen@ccc.net>
2025-12-22 12:51:44 +0200

    4.5 favorite blog and tests

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -6,7 +6,20 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    return blogs.reduce((maxLikesBlog, blog) => {
+        if (!maxLikesBlog.likes) {
+            maxLikesBlog = blog
+        }
+        if (blog.likes > maxLikesBlog.likes) {
+            maxLikesBlog = blog
+        }
+        return maxLikesBlog
+    }, {})
+}
+
 module.exports = {
   dummy, 
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
