7f6d51176ea7947a836910eb616cb0bdb7679c46
Gulcan Damdelen <zipwire1@hotmail.com>
2025-12-23 14:39:08 +0200

    4.14 Blog List Expansions, step 2

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -1,4 +1,6 @@
 const blogRouter = require('express').Router()
+const { request } = require('../app')
+const blog = require('../models/blog')
 const Blog = require('../models/blog')
 
 
@@ -22,6 +24,23 @@ blogRouter.delete('/:id', async (request, response) => {
   const id = request.params.id
   await Blog.findByIdAndDelete(id)
   response.status(204).end()
+})
+
+blogRouter.put('/:id', async (request, response) => {
+  const id = request.params.id
+  let blogToUpdate = await Blog.findById(id)
+
+  if (!blogToUpdate) {
+    return response.status(404).end()
+  } else {
+    const { title, url, likes, author } = request.body
+    blogToUpdate.title = title
+    blogToUpdate.url = url
+    blogToUpdate.likes = likes
+    blogToUpdate.author = author
+    blogToUpdate = await blogToUpdate.save()
+    return response.json(blogToUpdate)
+  }
 })
 
 module.exports = blogRouter
