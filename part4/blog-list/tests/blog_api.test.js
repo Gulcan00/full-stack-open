cea4d3e722224421a076b617b3d83ebf435a36c3
Gulcan Damdelen <gdamdelen@ccc.net>
2025-12-23 13:49:04 +0200

    4.10: Blog List Tests, step 3

--- part4/blog-list/tests/blog_api.test.js
+++ part4/blog-list/tests/blog_api.test.js
@@ -5,6 +5,7 @@ const app = require('../app')
 const assert = require('node:assert')
 const testHelper = require('./test_helper')
 const Blog = require('../models/blog')
+const { title } = require('node:process')
 
 const api = supertest(app)
 
@@ -30,6 +31,27 @@ test('blog posts have property id', async () => {
     const response = await api.get('/api/blogs')
 
     response.body.forEach(blog => assert(Object.hasOwn(blog, 'id')))
+})
+
+test('a valid blog can be added', async () => {
+    const blog = {
+        title: "Type wars 2",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+        likes: 0,
+    }
+
+    await api
+    .post('/api/blogs')
+    .send(blog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+    const blogs = await testHelper.blogsInDB()
+    assert.strictEqual(blogs.length, testHelper.initialBlogs.length + 1)
+
+    const titles = blogs.map(blog => blog.title)
+    assert(titles.includes('Type wars 2'))
 })
 
 after(() => {
