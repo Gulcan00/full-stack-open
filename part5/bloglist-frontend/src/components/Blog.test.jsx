fabe09176ddaceff39133b6395ad034bc088ce74
Gulcan Damdelen <zipwire1@hotmail.com>
2026-01-20 14:36:22 +0200

    5.15: Blog List Tests, step 3

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -42,3 +42,25 @@ test('displays URL and likes when view button is clicked', async () => {
     expect(url).toBeDefined()
     expect(likes).toBeDefined()
 })
+
+test('calls updateBlog twice when like button is clicked twice', async () => {
+    const blog = {
+        title: 'Test',
+        author: 'John Smith',
+        url: 'http://123.com',
+        likes: 10
+    }
+
+    const mockUpdateBlog = vi.fn()
+
+    render(<Blog blog={blog} updateBlog={mockUpdateBlog} />)
+
+    const user = userEvent.setup()
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
+    const likeButton = screen.getByText('like')
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+    expect(mockUpdateBlog).toHaveBeenCalledTimes(2)
+})
