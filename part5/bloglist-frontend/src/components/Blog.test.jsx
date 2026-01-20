b2178c3d530e8248ef3cb938943ed7f6d3be9fcb
Gulcan Damdelen <zipwire1@hotmail.com>
2026-01-20 14:34:56 +0200

    5.14: Blog List Tests, step 2

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
+import userEvent from '@testing-library/user-event'
 
 test('displays title and author, not URL or likes by default', () => {
     const blog = {
@@ -20,4 +21,24 @@ test('displays title and author, not URL or likes by default', () => {
     expect(author).toBeDefined()
     expect(url).toBeNull()
     expect(likes).toBeNull()
+})
+
+test('displays URL and likes when view button is clicked', async () => {
+    const blog = {
+        title: 'Test',
+        author: 'John Smith',
+        url: 'http://123.com',
+        likes: 10
+    }
+
+    render(<Blog blog={blog} />)
+
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const url = screen.getByText('http://123.com', { exact: false })
+    const likes = screen.getByText('likes 10', { exact: false })
+    expect(url).toBeDefined()
+    expect(likes).toBeDefined()
 })
