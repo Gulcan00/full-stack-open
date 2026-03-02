81357cbe94fb022582f544d3312a564f8c381254
Gulcan Damdelen <zipwire1@hotmail.com>
2026-03-02 12:00:25 +0200

    Exercise 6.21

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,10 +1,9 @@
-import AnecdoteForm from './components/AnecdoteForm'
 import { useQuery } from '@tanstack/react-query'
-import { getAnecdotes } from './requests'
+import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { getAnecdotes } from './requests'
 
 const App = () => {
-
   const handleVote = (anecdote) => {
     console.log('vote')
   }
--- part6/query-anecdotes/src/requests.js
+++ part6/query-anecdotes/src/requests.js
@@ -7,3 +7,19 @@ export const getAnecdotes = async () => {
 
     return response.json()
 }
+
+export const createAnecdote = async (newAnecdote) => {
+    const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(newAnecdote)
+    }
+
+    const response = await fetch('http://localhost:3001/anecdotes', options)
+
+    if (!response.ok) {
+        throw new Error('anecdote could not be created')
+    }
+
+    return response.json()
+}
