b38f3cf411dbbdc279e84755b71df19347bdfd1e
Gulcan Damdelen <zipwire1@hotmail.com>
2026-03-06 12:02:07 +0200

    7.5: Anecdotes and Hooks step 2

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -87,6 +87,11 @@ const CreateNew = (props) => {
           <input name='info' {...info} />
         </div>
         <button>create</button>
+        <button type='button' onClick={() => {
+          content.reset()
+          author.reset()
+          info.reset()
+        }}>reset</button>
       </form>
     </div>
   )
